14295fefe084dfa0761c7b224ca0076f41b1a8f1
bodong.yang <bodong.yang@tier4.jp>
2025-08-05 09:49:34 +0000

    file_table.utils: now prepare_* methods will raise PrepareEntryFailed exception

--- src/ota_image_libs/v1/file_table/utils.py
+++ src/ota_image_libs/v1/file_table/utils.py
@@ -18,12 +18,25 @@ import os
 import shutil
 import stat
 from pathlib import Path
+from typing import Any
 
 from ota_image_libs.common.model_spec import MsgPackedDict, StrOrPath
 
 from .db import DirTypedDict, NonRegularFileTypedDict, RegularFileTypedDict
 
 CANONICAL_ROOT = "/"
+
+
+class PrepareEntryFailed(Exception):
+    entry: Any
+    """The entry that caused the failure."""
+
+    def __init__(self, entry: Any, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.entry = entry
+
+    def __str__(self) -> str:
+        return f"failed to process {self.entry} due to: {self.__cause__}"
 
 
 def _set_xattr(path: StrOrPath, _in: MsgPackedDict) -> None:
@@ -39,65 +52,74 @@ def fpath_on_target(_canonical_path: StrOrPath, target_mnt: StrOrPath) -> Path:
 
 
 def prepare_dir(entry: DirTypedDict, *, target_mnt: StrOrPath) -> Path:
-    _target_on_mnt = fpath_on_target(entry["path"], target_mnt=target_mnt)
-    _target_on_mnt.mkdir(exist_ok=True, parents=True)
-    os.chown(_target_on_mnt, uid=entry["uid"], gid=entry["gid"])
-    os.chmod(_target_on_mnt, mode=entry["mode"])
-    if xattrs := entry["xattrs"]:
-        _set_xattr(_target_on_mnt, xattrs)
-    return _target_on_mnt
+    try:
+        _target_on_mnt = fpath_on_target(entry["path"], target_mnt=target_mnt)
+        _target_on_mnt.mkdir(exist_ok=True, parents=True)
+        os.chown(_target_on_mnt, uid=entry["uid"], gid=entry["gid"])
+        os.chmod(_target_on_mnt, mode=entry["mode"])
+        if xattrs := entry["xattrs"]:
+            _set_xattr(_target_on_mnt, xattrs)
+        return _target_on_mnt
+    except Exception as e:
+        raise PrepareEntryFailed(entry) from e
 
 
 def prepare_non_regular(
     entry: NonRegularFileTypedDict, *, target_mnt: StrOrPath
 ) -> Path:
-    _target_on_mnt = fpath_on_target(entry["path"], target_mnt=target_mnt)
-    if stat.S_ISLNK(entry["mode"]):
-        _symlink_target_raw = entry["meta"]
-        assert _symlink_target_raw, (
-            f"{dict(entry)} is symlink, but no symlink target is defined"
-        )
+    try:
+        _target_on_mnt = fpath_on_target(entry["path"], target_mnt=target_mnt)
+        if stat.S_ISLNK(entry["mode"]):
+            _symlink_target_raw = entry["meta"]
+            assert _symlink_target_raw, (
+                f"{dict(entry)} is symlink, but no symlink target is defined"
+            )
 
-        _symlink_target = _symlink_target_raw.decode("utf-8")
-        _target_on_mnt.symlink_to(_symlink_target)
+            _symlink_target = _symlink_target_raw.decode("utf-8")
+            _target_on_mnt.symlink_to(_symlink_target)
 
-        # NOTE(20241213): chown will reset the sticky bit of the file!!!
-        #   Remember to always put chown before chmod !!!
-        os.chown(
-            _target_on_mnt,
-            uid=entry["uid"],
-            gid=entry["gid"],
-            follow_symlinks=False,
-        )
-        # NOTE: changing mode of symlink is not needed and uneffective, and on some platform
-        #   changing mode of symlink will even result in exception raised.
-    elif stat.S_ISCHR(entry["mode"]):
-        # NOTE: we only support placeholder char file with 0,0 devnode.
-        os.mknod(_target_on_mnt, mode=entry["mode"] | stat.S_IFCHR, device=0)
-        os.chown(
-            _target_on_mnt,
-            uid=entry["uid"],
-            gid=entry["gid"],
-            follow_symlinks=False,
-        )
-    else:
-        return _target_on_mnt  # silently ignore unknown file type
+            # NOTE(20241213): chown will reset the sticky bit of the file!!!
+            #   Remember to always put chown before chmod !!!
+            os.chown(
+                _target_on_mnt,
+                uid=entry["uid"],
+                gid=entry["gid"],
+                follow_symlinks=False,
+            )
+            # NOTE: changing mode of symlink is not needed and uneffective, and on some platform
+            #   changing mode of symlink will even result in exception raised.
+        elif stat.S_ISCHR(entry["mode"]):
+            # NOTE: we only support placeholder char file with 0,0 devnode.
+            os.mknod(_target_on_mnt, mode=entry["mode"] | stat.S_IFCHR, device=0)
+            os.chown(
+                _target_on_mnt,
+                uid=entry["uid"],
+                gid=entry["gid"],
+                follow_symlinks=False,
+            )
+        else:
+            return _target_on_mnt  # silently ignore unknown file type
 
-    if xattrs := entry["xattrs"]:
-        _set_xattr(_target_on_mnt, xattrs)
-    return _target_on_mnt
+        if xattrs := entry["xattrs"]:
+            _set_xattr(_target_on_mnt, xattrs)
+        return _target_on_mnt
+    except Exception as e:
+        raise PrepareEntryFailed(entry) from e
 
 
 def prepare_regular_copy(
     entry: RegularFileTypedDict, _rs: StrOrPath, *, target_mnt: StrOrPath
 ) -> Path:
-    _target_on_mnt = fpath_on_target(entry["path"], target_mnt=target_mnt)
-    shutil.copyfile(_rs, _target_on_mnt, follow_symlinks=False)
-    os.chown(_target_on_mnt, uid=entry["uid"], gid=entry["gid"])
-    os.chmod(_target_on_mnt, mode=entry["mode"])
-    if _xattr := entry["xattrs"]:
-        _set_xattr(_target_on_mnt, _in=_xattr)
-    return _target_on_mnt
+    try:
+        _target_on_mnt = fpath_on_target(entry["path"], target_mnt=target_mnt)
+        shutil.copyfile(_rs, _target_on_mnt, follow_symlinks=False)
+        os.chown(_target_on_mnt, uid=entry["uid"], gid=entry["gid"])
+        os.chmod(_target_on_mnt, mode=entry["mode"])
+        if _xattr := entry["xattrs"]:
+            _set_xattr(_target_on_mnt, _in=_xattr)
+        return _target_on_mnt
+    except Exception as e:
+        raise PrepareEntryFailed(entry) from e
 
 
 def prepare_regular_hardlink(
@@ -107,24 +129,30 @@ def prepare_regular_hardlink(
     target_mnt: StrOrPath,
     hardlink_skip_apply_permission: bool = False,
 ) -> Path:
-    # NOTE: os.link will make dst a hardlink to src.
-    _target_on_mnt = fpath_on_target(entry["path"], target_mnt=target_mnt)
-    os.link(_rs, _target_on_mnt)
-    if not hardlink_skip_apply_permission:
-        os.chown(_target_on_mnt, uid=entry["uid"], gid=entry["gid"])
-        os.chmod(_target_on_mnt, mode=entry["mode"])
-        if _xattr := entry["xattrs"]:
-            _set_xattr(_target_on_mnt, _in=_xattr)
-    return _target_on_mnt
+    try:
+        # NOTE: os.link will make dst a hardlink to src.
+        _target_on_mnt = fpath_on_target(entry["path"], target_mnt=target_mnt)
+        os.link(_rs, _target_on_mnt)
+        if not hardlink_skip_apply_permission:
+            os.chown(_target_on_mnt, uid=entry["uid"], gid=entry["gid"])
+            os.chmod(_target_on_mnt, mode=entry["mode"])
+            if _xattr := entry["xattrs"]:
+                _set_xattr(_target_on_mnt, _in=_xattr)
+        return _target_on_mnt
+    except Exception as e:
+        raise PrepareEntryFailed(entry) from e
 
 
 def prepare_regular_move(
     entry: RegularFileTypedDict, _rs: StrOrPath, *, target_mnt: StrOrPath
 ) -> Path:
-    _target_on_mnt = fpath_on_target(entry["path"], target_mnt=target_mnt)
-    os.replace(str(_rs), _target_on_mnt)
-    os.chown(_target_on_mnt, uid=entry["uid"], gid=entry["gid"])
-    os.chmod(_target_on_mnt, mode=entry["mode"])
-    if _xattr := entry["xattrs"]:
-        _set_xattr(_target_on_mnt, _in=_xattr)
-    return _target_on_mnt
+    try:
+        _target_on_mnt = fpath_on_target(entry["path"], target_mnt=target_mnt)
+        os.replace(str(_rs), _target_on_mnt)
+        os.chown(_target_on_mnt, uid=entry["uid"], gid=entry["gid"])
+        os.chmod(_target_on_mnt, mode=entry["mode"])
+        if _xattr := entry["xattrs"]:
+            _set_xattr(_target_on_mnt, _in=_xattr)
+        return _target_on_mnt
+    except Exception as e:
+        raise PrepareEntryFailed(entry) from e
