08193b63bd0be59b404033da1b78113990374856
bodong.yang <bodong.yang@tier4.jp>
2026-06-16 13:13:25 +0900

    jwt_utils: add helpers for working with AWS KMS signing

--- src/ota_image_libs/_crypto/jwt_utils.py
+++ src/ota_image_libs/_crypto/jwt_utils.py
@@ -17,6 +17,39 @@ from __future__ import annotations
 from typing import Any
 
 import jwt
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurve
+from jwt.utils import der_to_raw_signature
+
+JWT_ALG_AWS_ALG_MAPPING = {
+    # JWT alg: AWS sign algorithm
+    "ES256": "ECDSA_SHA_256",
+    "ES384": "ECDSA_SHA_384",
+    "ES512": "ECDSA_SHA_512",
+}
+"""Mapping between JWT ES* series algorithm to AWS KMS signing algorithm set.
+
+Also see https://docs.aws.amazon.com/kms/latest/APIReference/API_Sign.html#API_Sign_RequestSyntax
+and https://datatracker.ietf.org/doc/html/rfc7518#section-3.4.
+"""
+
+AWS_ALG_JWT_ALG_MAPPING = {v: k for k, v in JWT_ALG_AWS_ALG_MAPPING.items()}
+
+
+def ec_sign_der_to_raw_signature(der_sig: bytes, ec_curve: EllipticCurve):
+    """Util to convert a DER-format ECDSA sign to JWT signature format.
+
+    JWT expects the signature to be in "raw" format, see https://datatracker.ietf.org/doc/html/rfc7518#section-3.4.
+    ECDSA signing request response from AWS KMS sign is in DER format, so this convert is required.
+    See https://docs.aws.amazon.com/kms/latest/APIReference/API_Sign.html#API_Sign_ResponseSyntax.
+    """
+    return der_to_raw_signature(der_sig, ec_curve)
+
+
+def get_aws_sign_alg(jwt_sign_alg: str) -> str:
+    try:
+        return JWT_ALG_AWS_ALG_MAPPING[jwt_sign_alg]
+    except KeyError:
+        raise ValueError(f"unsupported {jwt_sign_alg=}") from None
 
 
 def compose_jwt(
